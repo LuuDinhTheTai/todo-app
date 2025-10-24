9bb642ea82cced9975b77293d2ab49f5e0a94f0b
Nguyen Van Tam <nguyentamm2601@gmail.com>
2025-10-24 21:56:40 +0700

    Add DueDate property to Todo class
    Added DueDate property to Todo class.

--- todo-app/entity/Todo.cs
+++ todo-app/entity/Todo.cs
@@ -1,9 +1,10 @@
-﻿namespace todo_app.entity;
+namespace todo_app.entity;
 
 public class Todo
 {
     public int Id { get; set; }
     public string Content { get; set; } = string.Empty;
     public bool IsDone { get; set; } = false;
+    public DateTime? DueDate { get; set; }
     public ICollection<Tag> Tags { get; set; } = new List<Tag>();
 }
