698ed3b259dd7be33726962f35584e22b3577814
Nguyen Van Tam <nguyentamm2601@gmail.com>
2025-10-26 23:35:21 +0700

    Create ChartForm.Designer.cs

---
+++ todo-app/view/ChartForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace todo_app.view
+{
+    partial class ChartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            cPie = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)cPie).BeginInit();
+            SuspendLayout();
+            // 
+            // cPie
+            // 
+            chartArea1.Name = "ChartArea1";
+            cPie.ChartAreas.Add(chartArea1);
+            cPie.Dock = DockStyle.Fill;
+            legend1.Name = "Legend1";
+            cPie.Legends.Add(legend1);
+            cPie.Location = new Point(0, 0);
+            cPie.Name = "cPie";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            cPie.Series.Add(series1);
+            cPie.Size = new Size(800, 450);
+            cPie.TabIndex = 0;
+            cPie.Text = "Thống kê";
+            // 
+            // ChartForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(cPie);
+            Name = "ChartForm";
+            Text = "ChartForm";
+            Load += ChartForm_Load;
+            ((System.ComponentModel.ISupportInitialize)cPie).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart cPie;
+    }
+}
