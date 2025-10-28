dd6aefd746f02eace2c3776d38352038aa4a465e
Luu Dinh The Tai <luudinthetai741@gmail.com>
2025-10-28 22:01:46 +0700

    Revert "Merge pull request #14 from NSQ266/priority"
    This reverts commit 87bf57af7cee7781b7f5996d7631434df17e17f6, reversing
    changes made to 2974aeeb6215361d20896357ccffdd435a457773.

--- todo-app/service/TodoService.cs
+++ todo-app/service/TodoService.cs
@@ -1,6 +1,5 @@
 ﻿using todo_app.controller;
 using todo_app.entity;
-using todo_app.exception;
 using todo_app.repository;
 
 namespace todo_app.service;
@@ -14,14 +13,12 @@ public class TodoService
         _todoRepository = controller.TodoRepository;
     }
     
-    public void Create(string content, Tag tag, int priority = 1)
+    public void Create(string content, Tag tag)
     {
         Todo todo = new Todo();
         todo.Content = content;
         todo.Tags.Add(tag);
-        todo.Priority = priority;
         _todoRepository.Create(todo);
-        _todoRepository.AddTag(todo.Id, tag.Id);
     }
     
     public List<Todo> FindByTagId(int tagId)
@@ -32,32 +29,6 @@ public class TodoService
     public void Update(Todo newTodo)
     {
         _todoRepository.Update(newTodo);
-    }
-
-    public void UpdateTodoPriority(int todoId, int newPriority)
-    {
-        var todo = _todoRepository.FindById(todoId); 
-        if (todo == null) throw new AppException("Todo không tồn tại");
-
-        todo.Priority = newPriority;
-        _todoRepository.Update(todo);
-    }
-
-    public Todo? FindById(int id)
-    {
-        return _todoRepository.FindById(id);
-    }
-
-    public void UpdateTodoFull(Todo todo)
-    {
-        // dùng khi update content, status, note, priority...
-        _todoRepository.Update(todo);
-    }
-
-    public ICollection<Todo> GetTodosForTag(Tag tag)
-    {
-        if (tag == null) throw new AppException("Chưa chọn danh sách");
-        return _todoRepository.FindByTagId(tag.Id);
     }
 
     public void Delete(int id)
