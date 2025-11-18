e52f4b98ceee250d7794161f43cd33d92597a229
Nguyen Van Tam <nguyentamm2601@gmail.com>
2025-11-18 19:26:17 +0700

    Implement remember me feature in LoginForm
    Add remember me functionality to the login process.

--- todo-app/view/LoginForm.cs
+++ todo-app/view/LoginForm.cs
@@ -1,13 +1,13 @@
-﻿using todo_app.controller;
+using todo_app.controller;
 
 namespace todo_app;
 
 public partial class LoginForm : Form
 {
     private Controller _controller;
-    
+
     private MainForm _mainForm;
-    
+
     public LoginForm(Controller controller)
     {
         InitializeComponent();
@@ -27,10 +27,19 @@ public partial class LoginForm : Form
         string password = tBPassword.Text;
         _controller.AccountService.Login(username, password);
 
+        if (cbRememberMe.Checked)
+        {
+            _controller.RememberMeService.Save(username, password);
+        }
+        else
+        {
+            _controller.RememberMeService.Clear();
+        }
+
         MessageBox.Show("Đăng nhập thành công!", "Thông báo", MessageBoxButtons.OK);
-        
+
         Close();
-        
+
         _mainForm.Reload();
         _mainForm.Show();
     }
