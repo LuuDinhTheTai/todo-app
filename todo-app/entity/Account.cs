32a7b2286388db5d98052eac10659081cccf2765
Luu Dinh The Tai <luudinthetai741@gmail.com>
2025-11-20 14:54:02 +0700

    refactor: update entities

--- todo-app/entity/Account.cs
+++ todo-app/entity/Account.cs
@@ -1,9 +1,10 @@
-﻿namespace todo_app.entity;
+﻿using System.Collections.Generic;
+
+namespace todo_app.entity;
 
 public class Account
 {
     public int Id { get; set; }
     public string Username { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
-    public ICollection<Tag> Tags { get; set; } = new List<Tag>();
 }
--- todo-app/entity/Tag.cs
+++ todo-app/entity/Tag.cs
@@ -1,8 +1,10 @@
-﻿namespace todo_app.entity;
+﻿using System.Collections.Generic;
+
+namespace todo_app.entity;
 
 public class Tag
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public int? AccountId { get; set; }
+    public int AccountId { get; set; }
 }
