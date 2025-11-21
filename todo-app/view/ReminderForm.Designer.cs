01b09760ddde500fdec223ff8f0df718622b4001
Luu Dinh The Tai <luudinthetai741@gmail.com>
2025-11-21 22:44:03 +0700

    .

---
+++ todo-app/view/ReminderForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace todo_app.view
+{
+    partial class ReminderForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblReminderText = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblReminderText
+            // 
+            this.lblReminderText.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblReminderText.Location = new System.Drawing.Point(12, 9);
+            this.lblReminderText.Name = "lblReminderText";
+            this.lblReminderText.Size = new System.Drawing.Size(358, 78);
+            this.lblReminderText.TabIndex = 0;
+            this.lblReminderText.Text = "Reminder content goes here.";
+            this.lblReminderText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(144, 90);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(94, 29);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "OK";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // ReminderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 131);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblReminderText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ReminderForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lời nhắc";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Label lblReminderText;
+        private Button btnClose;
+    }
+}
